0207425ee9ea3de5de6200f02ef9072155b454c1
Rafael de sousa <161399465+rafaelvon1@users.noreply.github.com>
2025-03-14 23:00:44 -0300

    estrutura melhorada

---
+++ bd/banco-de-dados-willian.sql
@@ -0,0 +1,6 @@
+/*-------------criando database e tabela--------------*/
+create database ProjetoJava;
+use ProjetoJava;
+create table teste(email varchar(50) not null, senha varchar(50) not null);
+
+select * from teste;
